b7596b16a2f0e40218d91cd96009120212833ba1
Alex Al <spikeal8@gmail.com>
2026-09-10 13:33:16 +0300

    feat(m1): export measurement runtime adapter

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './contracts';
 export * from './application/CadApplicationImpl';
 export * from './runtime/OpenCascadePartRuntime';
+export * from './runtime/OpenCascadeMeasurementAdapter';
 export * from './runtime/PlaneGCSSketchSolverRuntime';
 export * from './host/MemoryCadProjectHost';
