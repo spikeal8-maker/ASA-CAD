243249f7b3a9f22db0cf7e2e63bb20fce9cb62af
Alex Al <spikeal8@gmail.com>
2026-09-10 19:04:13 +0300

    feat(m2i): centralize viewport visual tokens

---
+++ src/web/viewportTokens.ts
@@ -0,0 +1,21 @@
+export const VIEWPORT_VISUAL_TOKENS = Object.freeze({
+  solid: 0xc8d3df,
+  facePreselection: 0xa9d7f5,
+  faceSelection: 0x66b9e8,
+  edge: 0x4b5966,
+  edgePreselection: 0x35a7dd,
+  edgeSelection: 0x0f82c1,
+  markerPreselection: 0x35a7dd,
+  markerSelection: 0x0f82c1,
+  hemisphereSky: 0xffffff,
+  hemisphereGround: 0x657080,
+  light: 0xffffff,
+});
+
+export const VIEWPORT_INTERACTION = Object.freeze({
+  orbitRadiansPerPixel: 0.006,
+  wheelZoomExponent: 0.0012,
+  minDistanceFactor: 0.02,
+  maxDistanceFactor: 50,
+  rightDragThresholdPx: 4,
+});
